b2b324047a5bf319f95000de8d82c2c37f7eabb9
Rajan Kumar <kumar9089rajan@gmail.com>
2024-01-28 15:54:29 +0530

    first commit

---
+++ projectDjkstra.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <climits>
+using namespace std;
+#define V 25
+int mindistance(int distance[], bool stat[])
+{
+    int minimum = INT_MAX, index;
+    for (int k = 0; k < V; k++)
+    {
+        if (stat[k] == false && distance[k] <= minimum) // stat = status
+        {
+            minimum = distance[k];
+            index = k;
+        }
+    }
+    return index;
+}
+
+void dijkstra(int graph[V][V], int source, string stations[V])
+{
+    int distance[53];
+    bool stat[53];
+    for (int k = 0; k < V; k++)
+    {
+        distance[k] = INT_MAX;
+        stat[k] = false;
+    }
+    distance[source] = 0;
+
+    for (int k = 0; k < V - 1; k++)
+    {
+        int m = mindistance(distance, stat);
+        stat[m] = true;
+        for (int i = 0; i < V; i++)
+        {
+            if (!stat[i] && graph[m][i] && distance[m] != INT_MAX && distance[m] + graph[m][i] < distance[i])
+                distance[i] = distance[m] + graph[m][i];
+        }
+    }
+    cout << "Minimum distance from  " << stations[source] << " to every station is given below: " << endl;
+    for (int k = 0; k < V; k++)
+    {
+        cout << "Minimum distance from " << stations[source] << " to " << stations[k] << " is " << distance[k] << " km. " << endl;
+    }
+}
+int main()
+{
+
+    int graph[V][V] = {{0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {9, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 16, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 5, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 9, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 5, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 12, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 2, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 5, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 6, 0, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 4, 0, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 8, 0, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 5, 0, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 6, 0},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 3},
+                       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0}};
+
+    int source;
+    string stations[V] = {"Muzaffarpur", "Jubba Sahani", "Pramajeevar Tarajeevar", "Benipurgram", "New Runnisaidpur", "Runnisaidpur", "Garha", "Dumra", "Bhisa", "Sitamarhi", "Kaparpura", "Kanti", "Piprahan", "Nariyar", "Motipur", "Mehwal", "Mehsi", "harpur Nag", "Chakia", "Kuria Halt", "pipra", "Bangari", "Jiwdhara", "Motihari Court", "Bapudham Motihari"};
+
+    cout << endl
+         << "Enter source station: " << endl;
+    for (int i = 0; i < V; i++)
+        cout << "Enter " << i << " for " << stations[i] << endl;
+    cin >> source;
+    dijkstra(graph, source, stations);
+    cout << endl;
+    return 0;
+}
